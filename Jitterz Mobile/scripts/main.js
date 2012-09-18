532216652e07f578d91378f1d7decd7dc79a8a77
jaxonsdaniels@gmail.com <>
2012-09-18 13:33:54 -0500

    Added announcements

--- Jitterz Mobile/scripts/main.js
+++ Jitterz Mobile/scripts/main.js
@@ -17,11 +17,24 @@ function onDeviceReady() {
 }
 
 function getLocation() {
-    navigator.geolocation.getCurrentPosition(onGeolocationSuccess, onGeolocationError, { enableHighAccuracy: true });
+	navigator.geolocation.getCurrentPosition(onGeolocationSuccess, onGeolocationError, { enableHighAccuracy: true });
 }
 
 function storesShow(e) {
-    getLocation();
+	getLocation();
+}
+
+var announcementData = [
+	{ title: "Holiday Drinks Are Here", description: "Enjoy your favorite holiday drinks, like Pumpkin Spice Lattes.", url: "images/holiday.jpg" },
+	{ title: "Register & Get Free Drinks", description: "Register any Jitterz card and start earning rewards like free drinks. Sign-up now.", url: "images/rewards.jpg" },
+	{ title: "Cheers to Another Year", description: "Raise a cup of bold and spicy Jitterz Anniversary Blend.", url: "images/anniversary.jpg" }
+];
+
+function announcementListViewTemplatesInit() {
+	$("#announcements-listview").kendoMobileListView({
+		dataSource: kendo.data.DataSource.create({ data: announcementData }),
+		template: $("#announcement-listview-template").html()
+	});
 }
 
 //=======================Geolocation Operations=======================//
@@ -31,7 +44,7 @@ function onGeolocationSuccess(position) {
 	// Use Google API to get the location data for the current coordinates
 	var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 	
-    var myOptions = {
+	var myOptions = {
 		zoom: 12,
 		center: latlng,
 		mapTypeControl: false,
@@ -47,29 +60,29 @@ function onGeolocationSuccess(position) {
 		title: "Your Location"
 	});
     
-    // Set property markers
+	// Set property markers
 	var locations = [];
     
 	// Get stores nearby
 	// http://www.starbucks.com/api/location.ashx?&features=&lat=47.58827590942383&long=-122.0338363647461&limit=10
 	$.getJSON("http://www.starbucks.com/api/location.ashx?&features=&lat=" + position.coords.latitude + "&long=" + position.coords.longitude + "&limit=10",
-        function(data) {
+			  function(data) {
             
-            $.each(data, function(i, item) {
+				  $.each(data, function(i, item) {
                 
-                locations.push(
-                {
-                    address: item.WalkInAddressDisplayStrings[0] + ", " + item.WalkInAddressDisplayStrings[1], 
-                    latlng: new google.maps.LatLng(item.WalkInAddress.Coordinates.Latitude, item.WalkInAddress.Coordinates.Longitude)
-                });                
-            });
-        })
-        .complete(function() {
-            setMarkers(locations);  
-        })
-	    .error(function(error) {
-		    alert(error.message);
-	    });
+					  locations.push(
+						  {
+						  address: item.WalkInAddressDisplayStrings[0] + ", " + item.WalkInAddressDisplayStrings[1], 
+						  latlng: new google.maps.LatLng(item.WalkInAddress.Coordinates.Latitude, item.WalkInAddress.Coordinates.Longitude)
+					  });                
+				  });
+			  })
+	.complete(function() {
+		setMarkers(locations);  
+	})
+	.error(function(error) {
+		alert(error.message);
+	});
 }
 
 // onGeolocationError Callback receives a PositionError object
@@ -88,7 +101,7 @@ function setMarkers(locations) {
 												new google.maps.Point(0, 0),
 												new google.maps.Point(12, 35));
     
-    var markerImage = new google.maps.MarkerImage('icons/coffeecupbutton.png');
+	var markerImage = new google.maps.MarkerImage('icons/coffeecupbutton.png');
 	var marker;
     
 	for (i = 0; i < locations.length; i++) {
